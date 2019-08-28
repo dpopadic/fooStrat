4163c8c2abe9ab2a41bffb7ba7da83de65d005b8
Dario Popadic <34684467+dpopadic@users.noreply.github.com>
2019-08-28 11:10:03 +0100

    update data_prep

--- data_prep.py
+++ data_prep.py
@@ -1,22 +1,29 @@
-
-
 import pandas as pd
-from pandas import ExcelWriter
-from pandas import ExcelFile
+import os
+# undo latest commit Alt + 9 -> log Tab -> select last commit
 
-df_tmp = pd.read_excel('data/all-euro-data-2018-2019.xlsx', sheet_name='E0')
+# single sheet visualisation..
+df_tmp = pd.read_excel('src_data/all-euro-data-2018-2019.xlsx', sheet_name='E0')
 df_tmp.head()
 
-df = pd.read_excel('data/all-euro-data-2018-2019.xlsx', sheet_name=None)
+# major leagues data
+# ---
+src_dat_path = os.path.join(os.getcwd(), 'src_data', '')
+fi_nm = ['src_data/' + f for f in os.listdir(src_dat_path) if f[:13] == 'all-euro-data']
 
-df_cons = pd.DataFrame.from_dict(df, orient='index')
-
-df_cons = pd.DataFrame([df], index='Div', column)
-
-df['E0']
-
-df_cons.head()
-df_cons.columns
+df = pd.DataFrame()
+for f in fi_nm:
+    df0 = pd.read_excel(f, sheet_name=None)
+    for key, i in df0.items():
+        df = df.append(i, ignore_index=True, sort=False)
+df.to_pickle('./pro_data/major_leagues.pkl')
 
 
+# niche leagues data
+# ---
+di = pd.read_excel('src_data/new_leagues_data.xlsx', sheet_name=None)
+df0 = pd.DataFrame()
+for key, i in di.items():
+    df0 = df0.append(i, ignore_index=True)
+df0.to_pickle('./pro_data/niche_leagues.pkl')
 
