59e6633877c2a3a3903b7c8c9fa40c4292d9a631
dpopadic <dario.popadic@yahoo.com>
2020-10-25 11:39:40 +0000

    est_odds_accuracy

--- fooStrat/testing.py
+++ fooStrat/testing.py
@@ -17,6 +17,7 @@ ik = data.query("div=='E0' & season=='2019' & date=='2020-07-22' & home_team=='l
 ik = data.query("div=='E0' & season=='2019' & date=='2019-08-09' & home_team=='liverpool' & field=='FTR'")
 
 from fooStrat.evaluation import reshape_wdl
+from fooStrat.evaluation import con_res_wd
 
 def feat_odds_uncertainty(data, odds):
     """Derives the prediction/odds uncertainty features.
@@ -51,22 +52,57 @@ df1['field'] = 'odds_volatility'
 # --- historical odds prediction uncertainty (hit ratio)
 # 1st approach: determine the lowest odds which define the most likely outcome
 # which odds? take max's -> as input since it takes a long time to retrieve
-data_ed = data[data['field'] == 'FTR'].reset_index(drop=True)
-# data_ed = reshape_wdl(data=data_ed, event='wdl', as_numeric=True)
-event_win = reshape_wdl(data=data_ed, event='win')
-event_lose = reshape_wdl(data=data_ed, event='lose')
-event_draw = reshape_wdl(data=data_ed, event='draw')
-event_wdl = pd.concat([event_win, event_lose, event_draw],
-                      axis=0,
-                      sort=True,
-                      ignore_index=True).reset_index(drop=True)
+event_wdl = con_res_wd(data=data, field='FTR', encoding=True)
 
 # merge results with odds
 mo = match_odds.pivot_table(index=['div', 'season', 'date', 'team'], columns='field', values='val').reset_index()
 rndo = pd.merge(event_wdl, mo, on = ['div', 'season', 'date', 'team'], how='left')
 
+
+
 # fit logit model
-a = rndo.query("div=='E0' & season=='2019' & team=='liverpool'").sort_values('date')
+a = rndo.query("div=='E0' & season=='2019' & team=='chelsea' & field=='win'").sort_values('date')
+a = rndo.query("div=='E0' & season=='2019' & team in ['chelsea', 'arsenal'] & field=='win'").sort_values('date')
+a = rndo.query("div=='E0' & season=='2019'").reset_index(drop=True)
+
+b = a.groupby(['div', 'season', 'team', 'field']).apply(est_odds_accuracy).reset_index()
+b = a.groupby(['div', 'season', 'team', 'field']).agg(est_odds_accuracy)
+
+
+est_odds_accuracy(a)
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+from fooStrat.helpers import class_accuracy_stats
+
+
+def est_odds_accuracy(data):
+    """Estimates odds accuracy for each team and season. Accuracy is defined here
+    as F1 score to take into account unbalanced outcomes.
+
+    Parameters:
+    -----------
+        data:   pd dataframe
+                data with columns odds_win, odds_draw, odds_lose & val (the outcome decoded in 0/1)
+    """
+    y = a['val'].values
+    X = a[['odds_win', 'odds_draw', 'odds_lose']].values
+    mod = LogisticRegression()
+    mod.fit(X, y)
+    y_h = mod.predict(X)
+    cm = confusion_matrix(y, y_h)
+    ca = class_accuracy_stats(cm).iloc[3,]
+    return ca
+
+
+
+
+
+
+
+
+
+
 
 
 
