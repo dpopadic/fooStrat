fc43ffbf88189bfd7913764c1f314950a14eaa44
dpopadic <dario.popadic@yahoo.com>
2020-11-16 16:53:56 +0000

    grouped estimation working example

--- fooStrat/testing.py
+++ fooStrat/testing.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
+from scipy.stats import zscore
 
 df = pd.DataFrame({
     'y': np.random.randn(20),
@@ -8,22 +9,13 @@ df = pd.DataFrame({
     'x2': np.random.randn(20),
     'grp': ['a', 'b'] * 10})
 
-def ols_res(x, y):
-    x_2d = x.values.reshape(len(x), -1)
-    return pd.Series(LinearRegression().fit(x_2d, y).predict(x_2d))
-
-df.groupby('grp').apply(lambda df: df[['x1', 'x2']].apply(ols_res, y=df['y']))
-
-
 def ols_res(x):
-    x_2d = x[['x1', 'x2']].values.reshape(len(x), -1)
+    xed = x[['x1', 'x2']].values.reshape(len(x), -1)
     y = x['y'].values
-    z = LinearRegression().fit(x_2d, y).predict(x_2d)
-    return pd.Series(z)
+    z = LinearRegression().fit(xed, y).predict(xed)
+    return pd.Series(z, index=x.index)
 
-df['yz'] = df.groupby('grp')['y'].transform(lambda x: zscore(x))
-
-df.groupby('grp').apply(lambda x: ols_res(x))
+df['pred'] = df.groupby('grp', as_index=False, group_keys=False).apply(lambda x: ols_res(x))
 
 
 
