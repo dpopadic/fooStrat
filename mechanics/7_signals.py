52922397a4a293bcf302ae8e046bdd853bff5e7f
dpopadic <dario.popadic@yahoo.com>
2020-12-30 11:16:48 +0000

    signals clean up

--- mechanics/7_signals.py
+++ mechanics/7_signals.py
@@ -4,9 +4,9 @@ import numpy as np
 from fooStrat.processing import fp_cloud
 import fooStrat.modelling as sm
 import fooStrat.evaluation as se
+import fooStrat.signals as si
 from fooStrat.response import con_res
 from fooStrat.servicers import con_est_dates, neutralise_field
-from fooStrat.signals import est_upcoming_proba, use_features, add_upcoming_date, register_predictions
 
 
 # DATA LOADING --------------------------------------------------------------------------------------------------------
@@ -19,17 +19,16 @@ flib = pd.read_pickle(fp_cloud + 'pro_data/flib_e0.pkl')
 # data reshaping for evaluation
 results = con_res(data=source_core, obj=['wdl'], event='win')
 dasetmod = sm.con_mod_datset_0(factors=flib, results=results)
-dasetmod = use_features(data=dasetmod)
+dasetmod = si.use_features(data=dasetmod)
 est_dates = con_est_dates(data=source_core, k=5, map_date=True, div=flib['div'].iloc[0])
 
-
 # upcoming games predictions
-pe = est_upcoming_proba(data=dasetmod,
-                        est_dates=est_dates,
-                        lookback='520W',
-                        categorical=['home'],
-                        models=['nb', 'knn'],
-                        show_expired=True)
+pe = si.est_upcoming_proba(data=dasetmod,
+                           est_dates=est_dates,
+                           lookback='520W',
+                           categorical=['home'],
+                           models=['nb', 'knn'],
+                           show_expired=True)
 # derive mispriced events
 oe = match_odds.query("field=='odds_win'").reset_index(drop=True).drop('field', axis=1)
 mo = sm.comp_mispriced(prob=pe,
@@ -37,8 +36,8 @@ mo = sm.comp_mispriced(prob=pe,
                        prob_threshold=0.3,
                        res_threshold=0.2)
 # real game date info
-mo = add_upcoming_date(data=mo, upcoming=ucg)
-register_predictions(data=mo, overwrite=False)
+mo = si.add_upcoming_date(data=mo, upcoming=ucg)
+si.register_predictions(data=mo, overwrite=False)
 
 
 
