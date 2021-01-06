2384d66404011973e8b45c345921a1e1baec6606
dpopadic <dario.popadic@yahoo.com>
2021-01-06 23:09:20 +0000

    adding setup for pkg installation

--- fooStrat/testing.py
+++ fooStrat/testing.py
@@ -4,7 +4,7 @@ from fooStrat.constants import fp_cloud_process
 df = pd.read_pickle(fp_cloud_process + 'flib_e0.pkl')
 fp = fp_cloud_process + 'etl_test_log.txt'
 myFile = open(fp, 'a+')
-myFile.write('\n' + str(df['date'].max()) + 'data processed on ' + str(datetime.now()))
+myFile.write('\n' + str(df['date'].max()) + ' | data processed on ' + str(datetime.now()))
 myFile.close()
 
 
---
+++ schedule/test_schedule_enh.py
@@ -0,0 +1,13 @@
+from crontab import CronTab
+cron = CronTab(user=True)
+py_inst = '/Users/dariopopadic/PycharmProjects/fooStrat/venv/bin/python3.7'
+py_file = '/Users/dariopopadic/PycharmProjects/fooStrat/fooStrat/testing.py'
+job = cron.new(command = py_inst + ' ' + py_file, comment = 'foostrat_etl')
+job.minute.every(1)
+cron.write()
+
+# view jobs:
+for job in cron:
+    print(job)
+
+
