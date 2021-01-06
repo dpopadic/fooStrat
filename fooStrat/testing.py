7160df88cc7aba30d1b89757a3deac720f781958
dpopadic <dario.popadic@yahoo.com>
2021-01-06 17:10:49 +0000

    automated run testing preps

--- fooStrat/testing.py
+++ fooStrat/testing.py
@@ -1,78 +1,11 @@
+from datetime import datetime
 import pandas as pd
-import numpy as np
-from sklearn.linear_model import LinearRegression
-from sklearn.naive_bayes import GaussianNB
-from scipy.stats import zscore
-
-fp_cloud = '/Users/dariopopadic/Library/Mobile Documents/com~apple~CloudDocs/Data/fooStrat/'
-
-ug = pd.read_pickle(fp_cloud + 'pro_data/upcoming_games.pkl')
-ug.query("div=='E0' & field=='AvgD'")
-
-
-df = pd.DataFrame({
-    'y': np.random.randn(20),
-    'x1': np.random.randn(20),
-    'x2': np.random.randn(20),
-    'grp': ['a', 'b'] * 10})
-
-def ols_res(x):
-    xed = x[['x1', 'x2']].values.reshape(len(x), -1)
-    y = x['y'].values
-    z = LinearRegression().fit(xed, y).predict(xed)
-    return pd.Series(z, index=x.index)
-
-df['pred'] = df.groupby('grp', as_index=False, group_keys=False).apply(lambda x: ols_res(x))
-
-
-df = pd.DataFrame({
-    'y': [0] * 10 + [1] * 10,
-    'x1': np.random.randn(20),
-    'x2': np.random.randn(20),
-    'grp': ['a', 'b'] * 10})
-x = df
-def nb_res(x):
-    xed = x[['x1', 'x2']].values.reshape(len(x), -1)
-    y = x['y'].values
-    z = GaussianNB().fit(xed, y).predict_proba(xed)[:, 1]
-    return pd.Series(z, index=x.index)
-
-df['pred'] = df.groupby('grp', as_index=False, group_keys=False).apply(lambda x: nb_res(x))
-
-
-
-
-
-a = dasetmod.query("date <= @t_pred").set_index('date').last('156W').reset_index()
-d = a.team.unique()
-b = a.query("team=='bournemouth'").reset_index(drop=True)
-
-for i in range(0, len(d)):
-    ik = d[i]
-    b = a.query("team==@ik").reset_index(drop=True)
-    X_train, X_test, y_train, meta_test = con_mod_datset_1(data=b,
-                                                           per_ind=per_ind,
-                                                           t_fit=t_fit,
-                                                           t_pred=t_pred,
-                                                           per='156W')
-    if len(X_train) < 1 or len(X_test) < 1:
-        est_proba = pd.DataFrame()
-    else:
-        # make no predictions if only 1 class (eg. win) is present in training set (revisit this later)
-        try:
-            z = GaussianNB().fit(X_train, y_train).predict_proba(X_test)[:, 1]
-            est_proba = pd.concat([meta_test, pd.DataFrame(z, columns=['val'])], axis=1)
-        except:
-            est_proba = pd.DataFrame()
-
-    print(ik)
-
-
-
-
-
-
-
+from fooStrat.constants import fp_cloud_process
+df = pd.read_pickle(fp_cloud_process + 'flib_e0.pkl')
+fp = fp_cloud_process + 'etl_test_log.txt'
+myFile = open(fp, 'a+')
+myFile.write('\n' + str(df['date'].max()) + 'data processed on ' + str(datetime.now()))
+myFile.close()
 
 
 
